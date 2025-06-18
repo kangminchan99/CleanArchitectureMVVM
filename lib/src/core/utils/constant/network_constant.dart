df214ac1c87773a2599cc7155b9f0ac1958a14b3
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2025-06-18 17:54:39 +0900

    api path

---
+++ lib/src/core/utils/constant/network_constant.dart
@@ -0,0 +1,12 @@
+import 'package:cleanarchitecture/src/core/utils/constant/config.dart';
+
+String apiUrl = '${Config.articleApiUrl}/mostviewed/';
+String apiKey = Config.articleApiKey;
+
+String getApiKeyPath() {
+  return '?api-key=$apiKey';
+}
+
+String getArticlesPath(int period) {
+  return 'all-sections/${period.toString()}.json${getApiKeyPath()}';
+}
