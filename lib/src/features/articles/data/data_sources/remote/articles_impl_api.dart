4453498cc25aec8dd9e186327dfc25ba582eb559
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2025-06-19 11:49:17 +0900

    추상 클래스를 실제로 구현하여, API에서 데이터를 가져오는 로직

--- lib/src/features/articles/data/data_sources/remote/articles_impl_api.dart
+++ lib/src/features/articles/data/data_sources/remote/articles_impl_api.dart
@@ -1,3 +1,5 @@
+import 'package:cleanarchitecture/src/core/network/error/dio_error_handler.dart';
+import 'package:cleanarchitecture/src/core/network/error/exceptions.dart';
 import 'package:cleanarchitecture/src/core/utils/constant/network_constant.dart';
 import 'package:cleanarchitecture/src/features/articles/data/data_sources/remote/abstract_article_api.dart';
 import 'package:cleanarchitecture/src/features/articles/domain/article_model.dart';
@@ -19,6 +21,24 @@ class ArticlesImplApi extends AbstractArticleApi {
   ) async {
     try {
       final result = (await dio.get(getArticlesPath(params.period)));
-    } 
+
+      if (result.data == null) {
+        throw ServerException('unknown error', result.statusCode);
+      }
+      return ApiResponse.fromJson<List<ArticleModel>>(
+        result.data,
+        ArticleModel.fromJsonList,
+      );
+    } on DioException catch (e) {
+      if (e.type == DioExceptionType.cancel) {
+        throw CancelTokenException(handleDioError(e), e.response?.statusCode);
+      } else {
+        throw ServerException(handleDioError(e), e.response?.statusCode);
+      }
+    } on ServerException {
+      rethrow;
+    } catch (e) {
+      throw ServerException(e.toString(), null);
+    }
   }
 }
