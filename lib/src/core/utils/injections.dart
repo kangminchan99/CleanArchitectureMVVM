448bf0d16f75850d7b395a89c0e6f012df3b8c49
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2025-06-19 14:14:00 +0900

    init injecgtions

--- lib/src/core/utils/injections.dart
+++ lib/src/core/utils/injections.dart
@@ -1,5 +1,6 @@
 import 'package:cleanarchitecture/src/core/network/dio_network.dart';
 import 'package:cleanarchitecture/src/core/utils/log/app_logger.dart';
+import 'package:cleanarchitecture/src/features/articles/articles_injections.dart';
 import 'package:cleanarchitecture/src/shared/app_injections.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -10,6 +11,7 @@ Future<void> initInjections() async {
   await initSharedPrefsInjections();
   await initAppInjections();
   await initDioInjections();
+  await initArticlesInjections();
 }
 
 // SharedPreferences 먼저 sl에 등록하고 initAppInjections에서 주입
