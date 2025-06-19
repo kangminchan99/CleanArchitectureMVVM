b9e507874bbc7b66cdc12c8cd74071ff5fbc6a3a
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2025-06-19 16:32:42 +0900

    intro page

--- lib/src/features/intro/presentation/pages/intro_page.dart
+++ lib/src/features/intro/presentation/pages/intro_page.dart
@@ -1,4 +1,8 @@
+import 'package:cleanarchitecture/src/core/helper/helper.dart';
+import 'package:cleanarchitecture/src/core/router/app_route_enum.dart';
+import 'package:cleanarchitecture/src/core/styles/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class IntroPage extends StatefulWidget {
   const IntroPage({super.key});
@@ -9,7 +13,33 @@ class IntroPage extends StatefulWidget {
 
 class _IntroPageState extends State<IntroPage> {
   @override
+  void initState() {
+    Future.delayed(Duration(seconds: 1), () {
+      if (!mounted) return;
+      Navigator.pushNamedAndRemoveUntil(
+        context,
+        AppRouteEnum.articlesPage.name,
+        (route) => false,
+      );
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(15.sp),
+          child: Image.asset(
+            Helper.getImgPath('logo.png'),
+            color: Helper.isDarkTheme() ? AppColors.white : null,
+            width: 300.w,
+            height: 300.h,
+            fit: BoxFit.contain,
+          ),
+        ),
+      ),
+    );
   }
 }
