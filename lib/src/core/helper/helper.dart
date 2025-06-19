22a23bd9f4d4ce27d7116312ac627fcbe3113796
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2025-06-19 14:04:18 +0900

    helper

--- lib/src/core/helper/helper.dart
+++ lib/src/core/helper/helper.dart
@@ -1,8 +1,45 @@
+import 'package:cleanarchitecture/src/core/utils/constant/app_constant.dart';
+import 'package:cleanarchitecture/src/core/utils/injections.dart';
+import 'package:cleanarchitecture/src/shared/data/data_sources/app_shared_prefs.dart';
 import 'package:cleanarchitecture/src/shared/domain/entities/language_enum.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Helper {
+  // get language
   static LanguageEnum getLang() {
     LanguageEnum? lang;
-    lang = sl<App>
+    lang = sl<AppSharedPrefs>().getLang();
+    lang = lang ?? LanguageEnum.en;
+    return lang;
+  }
+
+  // get svg img path
+  static String getSvgPath(String name) {
+    return '$svgPath$name';
+  }
+
+  // get img path
+  static String getImgPath(String name) {
+    return '$imagePath$name';
+  }
+
+  // get vertical space
+  static double getVerticalSpace() {
+    return 10.h;
+  }
+
+  // get horizontal space
+  static double getHorizontalSpace() {
+    return 10.w;
+  }
+
+  // get dio header
+  static Map<String, dynamic> getHeaders() {
+    return {}..removeWhere((key, value) => value == null);
+  }
+
+  // check if dark mode is enabled
+  static bool isDarkMode() {
+    return sl<AppSharedPrefs>().getIsDarkTheme();
   }
 }
