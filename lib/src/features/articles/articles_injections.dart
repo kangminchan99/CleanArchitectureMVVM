5324c4f4481bbd5b19f0dde66219c5d1575eec2d
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2025-06-19 13:47:22 +0900

    기사 인젝션

--- lib/src/features/articles/articles_injections.dart
+++ lib/src/features/articles/articles_injections.dart
@@ -1,6 +1,18 @@
+import 'package:cleanarchitecture/src/core/network/dio_network.dart';
 import 'package:cleanarchitecture/src/core/utils/injections.dart';
+import 'package:cleanarchitecture/src/features/articles/data/data_sources/local/articles_shared_prefs.dart';
+import 'package:cleanarchitecture/src/features/articles/data/data_sources/remote/articles_impl_api.dart';
+import 'package:cleanarchitecture/src/features/articles/data/repositories/arricles_repo_impl.dart';
+import 'package:cleanarchitecture/src/features/articles/domain/repositories/abstract_articles_repository.dart';
+import 'package:cleanarchitecture/src/features/articles/domain/usecases/articles_usecase.dart';
 
 initArticlesInjections() {
   // Article 관련 의존성 주입 설정
-  sl.registerSingleton(instance)
+  sl.registerSingleton<ArticlesImplApi>(ArticlesImplApi(DioNetwork.appAPI));
+  sl.registerSingleton<AbstractArticlesRepository>(
+    ArticlesRepositoryImpl(sl()),
+  );
+  sl.registerSingleton<ArticlesSharedPrefs>(ArticlesSharedPrefs(sl()));
+
+  sl.registerSingleton<ArticlesUsecase>(ArticlesUsecase(sl()));
 }
