32e62f2d675773a3ad4fcf0fa7256770dbe138a3
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2025-06-20 13:55:32 +0900

    shared background page

---
+++ lib/src/shared/presentation/pages/background_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class BackgroundPage extends StatefulWidget {
+  final Widget child;
+  final Color? backgroundColor;
+  final bool withDrawer;
+  final Function()? drawerCallBack;
+  final GlobalKey<ScaffoldState>? scaffoldKey;
+  final bool topSafeArea;
+  final bool bottomSafeArea;
+  final PreferredSizeWidget? appBar;
+  const BackgroundPage({
+    super.key,
+    required this.child,
+    this.backgroundColor,
+    this.withDrawer = false,
+    this.drawerCallBack,
+    this.scaffoldKey,
+    this.topSafeArea = true,
+    this.bottomSafeArea = true,
+    this.appBar,
+  });
+
+  @override
+  State<BackgroundPage> createState() => _BackgroundPageState();
+}
+
+class _BackgroundPageState extends State<BackgroundPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  bool isCallFromDrawer = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: widget.appBar,
+      key: widget.scaffoldKey,
+      backgroundColor:
+          widget.backgroundColor ?? Theme.of(context).scaffoldBackgroundColor,
+      resizeToAvoidBottomInset: false,
+      onDrawerChanged: (isOpened) {
+        if (!isOpened) {
+          if (!isCallFromDrawer) {
+            if (widget.drawerCallBack != null) {
+              widget.drawerCallBack!();
+            }
+          }
+        }
+      },
+      drawer: widget.withDrawer
+          ? Drawer(
+              backgroundColor: Theme.of(context).drawerTheme.backgroundColor,
+            )
+          : null,
+
+      body: SafeArea(
+        top: widget.topSafeArea,
+        bottom: widget.bottomSafeArea,
+        child: widget.child,
+      ),
+    );
+  }
+}
